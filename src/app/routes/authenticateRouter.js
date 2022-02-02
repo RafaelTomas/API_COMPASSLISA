dc1c463c673d0e949671546c632e8f9021b0ba27
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-02-01 23:57:36 -0300

    create validation

--- src/app/routes/authenticateRouter.js
+++ src/app/routes/authenticateRouter.js
@@ -1,7 +1,8 @@
 const authenticateController = require('../controllers/AuthenticateController');
+const validation = require('../validations/authenticate');
 
 module.exports = (server, routes, prefix = '/api/v1/authenticate') => {
-  routes.post('/', authenticateController.authenticate);
+  routes.post('/',validation ,authenticateController.authenticate);
   server.use(prefix, routes);
 
 };
--- src/app/validations/authenticate/index.js
+++ src/app/validations/authenticate/index.js
@@ -1,33 +1,19 @@
 const Joi = require('joi').extend(require('@joi/date'));
-const InvalidBody = require('../../errors/InvalidBody');
-const validateCPF = require('../../helpers/cpf');
+const invalidBody = require('../../errors/InvalidBody');
 
 const patternsPassword =/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/;
 const PassswordError = new Error('Password must be strong. At least one upper case alphabet. At least one lower case alphabet. At least one digit. At least one special character. Minimum six in length');
 
-const cpf = (cpf, helper) => {
-  if (validateCPF(cpf)) {
-    return helper.message('insert valid cpf');
-  } else {
-    return true;
-  }
-};
-
-const birthDate = new Date((Date.now()) - (1000 * 60 * 60 * 24 * 365 * 18));
 
 module.exports = async (req, res, next) => {
   try {
     const peopleSchema = Joi.object({
-      nome: Joi.string().min(3).trim().required(),
-      cpf: Joi.string().custom(cpf).unique().required(),
-      data_nascimento: Joi.date().format('DD/MM/YYYY').max(birthDate).required(),
       email: Joi.string().email({ minDomainSegments: 2 }).required(),
       senha: Joi.string().min(6).pattern(patternsPassword).error(PassswordError).required(),
-      habilitado: Joi.string().valid('sim', 'não').required()
     });
 
     const { error } = await peopleSchema.validate(req.body, { abortEarl: true });
-    if (error) throw new InvalidBody(error);
+    if (error) throw new invalidBody(error);
     return next();
   } catch (error) {
     return res.status(400).json({
