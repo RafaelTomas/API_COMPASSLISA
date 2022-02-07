0e219381cb0780fad10e974689d1347e89d15de4
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-02-07 19:56:43 -0300

    feat: swagger routerm

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,13 +1,15 @@
 const { Router } = require('express');
-const carRouter = require('../routes/carRouter.js');
-const peopleRouter = require('../routes/peopleRouter.js');
-const authenticateRouter = require('../routes/authenticateRouter.js');
+const carRouter = require('./carRouter');
+const peopleRouter = require('./peopleRouter.js');
+const authenticateRouter = require('./authenticateRouter.js');
+const swaggerRouter = require('./swaggerRouter.js');
 
 module.exports = server => {
   server.use((req, res, next) => {
     carRouter(server, new Router());
     peopleRouter(server, new Router());
     authenticateRouter(server, new Router());
+    swaggerRouter(server, new Router());
     next();
   });
 };
