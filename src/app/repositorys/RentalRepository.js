b923b48891b0f4f5dcaef059e09ba2f27e9572c4
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-02-08 11:58:23 -0300

    feat: create repository rental

---
+++ src/app/repositorys/RentalRepository.js
@@ -0,0 +1,41 @@
+const RentalSchema = require('../schemas/rentalSchemas');
+
+class RentalRepository {
+  async create(payload) {
+    return RentalSchema.create(payload);
+  }
+
+  async findall(payload) {
+    const myCustomLabels = {
+      totalDocs: 'total',
+      docs: 'Locadoras',
+      page: 'offset',
+      nextPage: false,
+      prevPage: false,
+      totalPages: 'offsets',
+      pagingCounter: false,
+      meta: false,
+      hasPrevPage: false,
+      hasNextPage: false,
+    };
+    const options = {
+      page: 1,
+      limit: 100,
+      customLabels: myCustomLabels,
+    };
+    return RentalSchema.paginate(payload, options, {});
+  }
+
+  async findId(id) {
+    return RentalSchema.findOne(id);
+  }
+
+  async delete(id) {
+    return RentalSchema.deleteOne(id);
+  }
+
+  async update(id) {
+    return RentalSchema.updateOne(id);
+  }
+}
+module.exports = new RentalRepository();
