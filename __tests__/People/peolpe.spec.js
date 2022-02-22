6cb7817ff1f5b93636247c0847fd45ce8c0d2f29
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-02-21 21:48:55 -0300

    fix: test delete not working

--- __tests__/People/peolpe.spec.js
+++ __tests__/People/peolpe.spec.js
@@ -1,7 +1,6 @@
 const request = require('supertest');
 const app = require('../../src/app');
 const peopleService = require('../../src/app/services/PeopleService');
-
 const truncate = require('../utils/truncate');
 
 const people = {};
@@ -10,9 +9,7 @@ describe('PEOPLE', () => {
   beforeEach(async () => {
     await truncate();
   });
-  /*
-   CRUD
-  */
+  /*   CRUD  */
   it('POST - CREATE PEOPLE', async () => {
     const res = await request(app)
       .post('/api/v1/people/')
@@ -26,15 +23,14 @@ describe('PEOPLE', () => {
       });
     expect(res.statusCode).toBe(200);
   });
-
   it('GET - FIND ALL', async () => {
     const res = await request(app)
       .get('/api/v1/people');
     expect(res.statusCode).toBe(200);
   });
 
-  it('PUT - UPDATE', async () => {
-    people.p5 = await peopleService.create({
+  it('GET - FIND ONE', async () => {
+    people.p4 = await peopleService.create({
       nome: 'Tomás Santos',
       cpf: '231.410.874-40',
       data_nascimento: '24/03/2003',
@@ -43,7 +39,21 @@ describe('PEOPLE', () => {
       habilitado: 'sim',
     });
     const res = await request(app)
-      .put(`/api/v1/people/${people.p5._id}`)
+      .get(`/api/v1/people/${people.p4._id}`);
+    expect(res.statusCode).toBe(200);
+  }); 
+
+  it('PUT - UPDATE', async () => {
+    people.p1 = await peopleService.create({
+      nome: 'Tomás Santos',
+      cpf: '231.410.874-40',
+      data_nascimento: '24/03/2003',
+      email: 'tomascompasso@gmail.com',
+      senha: 'Rafael@12',
+      habilitado: 'sim',
+    });
+    const res = await request(app)
+      .put(`/api/v1/people/${people.p1._id}`)
       .send(
         {
           nome: 'Rafael Tomás',
