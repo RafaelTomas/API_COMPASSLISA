8cfc0b5dbf7fe6e1b346707a71b2194cafc6caed
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-02-23 15:12:20 -0300

    fix: reserve

--- src/app/routes/reserveRouter.js
+++ src/app/routes/reserveRouter.js
@@ -1,11 +1,11 @@
-const ReserveController = require('../controllers/ReserveController');
-const validationId = require('../validations/id');
+// const ReserveController = require('../controllers/ReserveController');
+// const validationId = require('../validations/id');
 
-module.exports = (server, routes, prefix = '/api/v1/rental/:id/reserve') => {
-  routes.post('/', ReserveController.create);
-  routes.get('/', ReserveController.find);
-  routes.get('/:_id', validationId, ReserveController.findById);
-  routes.put('/:_id', validationId, ReserveController.update);
-  routes.delete('/:_id', validationId, ReserveController.delete);
-  server.use(prefix, routes);
-};
+// module.exports = (server, routes, prefix = '/api/v1/rental/:id/reserve') => {
+//   routes.post('/', ReserveController.create);
+//   routes.get('/', ReserveController.find);
+//   routes.get('/:_id', validationId, ReserveController.findById);
+//   routes.put('/:_id', validationId, ReserveController.update);
+//   routes.delete('/:_id', validationId, ReserveController.delete);
+//   server.use(prefix, routes);
+// };
