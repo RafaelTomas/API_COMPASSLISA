95dcd4439a086c601c64940a69d97e1adbba608c
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-02-23 15:15:34 -0300

    fix: reserve

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -4,13 +4,13 @@ const peopleRouter = require('./peopleRouter');
 const authenticateRouter = require('./authenticateRouter');
 const swaggerRouter = require('./swaggerRouter.js');
 const rentalRouter = require('./rentalRouter');
-const reserveRouter = require('./reserveRouter');
+// const reserveRouter = require('./reserveRouter');
 
 module.exports = (server) => {
   server.use((req, res, next) => {
     carRouter(server, new Router());
     peopleRouter(server, new Router());
-    reserveRouter(server, new Router());
+    // reserveRouter(server, new Router());
     rentalRouter(server, new Router());
     authenticateRouter(server, new Router());
     swaggerRouter(server, new Router());
--- src/app/services/ReserveService.js
+++ src/app/services/ReserveService.js
@@ -1,28 +1,28 @@
-const ReserveRepository = require('../repositorys/ReserveRepository');
+// const ReserveRepository = require('../repositorys/ReserveRepository');
 
-class ReserveService {
-  async create(payload) {
-    const data = await ReserveRepository.create(payload);
-    return data;
-  }
+// class ReserveService {
+//   async create(payload) {
+//     const data = await ReserveRepository.create(payload);
+//     return data;
+//   }
 
-  async find(payload) {
-    const data = await ReserveRepository.findall(payload);
-    return data;
-  }
+//   async find(payload) {
+//     const data = await ReserveRepository.findall(payload);
+//     return data;
+//   }
 
-  async findById(id) {
-    return ReserveRepository.findId(id);
-  }
+//   async findById(id) {
+//     return ReserveRepository.findId(id);
+//   }
 
-  async delete(id) {
-    return ReserveRepository.delete(id);
-  }
+//   async delete(id) {
+//     return ReserveRepository.delete(id);
+//   }
 
-  async update(id) {
-    const data = await ReserveRepository.update(id);
-    return data;
-  }
-}
+//   async update(id) {
+//     const data = await ReserveRepository.update(id);
+//     return data;
+//   }
+// }
 
-module.exports = new ReserveService();
+// module.exports = new ReserveService();
